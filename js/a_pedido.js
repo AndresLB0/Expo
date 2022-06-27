86f51ab76121d31973a2399197d7a11ae1698dc5
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-06-27 10:29:46 -0600

    a

--- js/a_pedido.js
+++ js/a_pedido.js
@@ -1,10 +1,17 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-    //   script para el combobox
-    var instances = M.FormSelect.init(document.querySelectorAll('select'));
-    var instances = M.Tabs.init(document.querySelector('.tabs', {
-      swipeable: true,
-    }));
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  //   script para el combobox
+  var instances = M.FormSelect.init(document.querySelectorAll('select'));
+  var instances = M.Tabs.init(document.querySelector('.tabs', {
+    swipeable: true,
+  }));
+});
 
-  document.getElementById('menu').innerHTML ='<ul id="tabs-swipe-demo" class="tabs"><li class="tab col s3"><a href="#pestania1">informacion del pedido</a></li><li class="tab col s3"><a href="#pestania2">detallar pedido</a></li></ul>';
+    document.getElementById('menu').innerHTML = `<ul id="tabs-swipe-demo" class="tabs">
+  <li class="tab col s3">
+    <a href="#pestania1">informacion del pedido</a>
+    </li>
+    <li class="tab col s3">
+      <a href="#pestania2">detallar pedido</a>
+      </li>
+      </ul>;`
