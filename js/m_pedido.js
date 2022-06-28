2e06eecaeeac959a9e175ca2cacef4dd91b53124
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-06-28 12:58:57 -0600

    10 changed files

--- js/m_pedido.js
+++ js/m_pedido.js
@@ -5,21 +5,3 @@
   }));
   var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'));
   });
-  //feacha y hora
-  function reloj(){
-  fecha = new Date();
-  anno = fecha.getFullYear();
-  mes = fecha.getMonth()+1;
-  dia=fecha.getDate();
-  diasem = fecha.getDay();
-  document.getElementsByClassName("hoy").innerHTML = dia+"/"+mes+'/'+anno;
-  //hora actual
- tiempo = new Date();
-hora = tiempo.getHours();
-if(hora<10)hora="0"+hora
- min = tiempo.getMinutes();
-if(min<10)min="0"+min;
-horacompleta=hora+":"+min;
-  document.getElementById('hora').innerHTML=horacompleta ;
-  setTimeout('reloj()',1000)
-  }
