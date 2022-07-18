a6a0fb97c388edd9d9e48869a459c56132d5eec5
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-07-18 09:19:39 -0600

    cambios333

---
+++ js/m_cliente.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //   script para el combobox
+    var instances = M.FormSelect.init(document.querySelectorAll('select'));
+    var instances = M.Tabs.init(document.querySelector('.tabs', {
+      swipeable: true,
+    }));
+  });
