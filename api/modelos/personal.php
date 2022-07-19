47e65641476de228d88e0ba883688036c1836170
AndresLB0 <86123811+AndresLB0@users.noreply.github.com>
2022-07-19 15:36:16 -0600

    Update personal.php

--- api/modelos/personal.php
+++ api/modelos/personal.php
@@ -145,13 +145,82 @@ public function setDireccion($value)
 
     public function createRow()
     {
-        $sql = 'INSERT INTO personal(nombre,dui,telefono,direccion,usuario,clave,nombre_cargo)
+        $sql = 'INSERT INTO personal(nombre,dui,telefono,direccion,usuario,clave,id_cargo)
                 VALUES(?, ?, ?, ?, ?, ?, ?)';
         $params = array($this->nombre, $this->contacto, $this->telefono);
         return Database::executeRow($sql, $params);
     }
+    public function readAll()
+    {
+        $sql = 'SELECT id_personal, nombre,dui,telefono,direccion,usuario,clave,nombre_cargo
+                FROM personal INNER JOIN cargo USING (id_cargo)
+                ORDER BY nombre';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
 
+    public function readOne()
+    {
+        $sql = 'SELECT id_personal, nombre,dui,telefono,direccion,usuario,clave,nombre_cargo
+        FROM personal INNER JOIN cargo USING (id_cargo)
+                WHERE id_personal = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM personal
+                WHERE id_personal = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
 
+    public function checkUser($usuario)
+    {
+        $sql = 'SELECT id_personal FROM personal WHERE usuario = ?';
+        $params = array($usuario);
+        if ($data = Database::getRow($sql, $params)) {
+            $this->id = $data['id_usuario'];
+            $this->usuario = $usuario;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function checkPassword($password)
+    {
+        $sql = 'SELECT clave FROM personal WHERE id_personal = ?';
+        $params = array($this->id);
+        $data = Database::getRow($sql, $params);
+        // Se verifica si la contraseña coincide con el hash almacenado en la base de datos.
+        if (password_verify($password, $data['clave'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function changePassword()
+    {
+        $sql = 'UPDATE personal SET clave = ? WHERE id_personal = ?';
+        $params = array($this->clave, $_SESSION['id_personal']);
+        return Database::executeRow($sql, $params);
+    }
 
+    public function readProfile()
+    {
+        $sql = 'SELECT id_personal,nombre,dui,telefono,direccion,usuario,clave,nombre_cargo
+                FROM personal
+                WHERE id_personal = ?';
+        $params = array($_SESSION['id_personal']);
+        return Database::getRow($sql, $params);
+    }
 
+    public function editProfile()
+    {
+        $sql = 'UPDATE personal
+                SET nombre =? ,dui=?,telefono=?,direccion=?,usuario=?,clave=?,nombre_cargo=?
+                WHERE id_personal = ?';
+        $params = array($this->nombre, $this->dui, $this->telefono, $_SESSION['id_personal']);
+        return Database::executeRow($sql, $params);
+    }
 }
